b40001d27556459ccdf262a1d22a0db287d09a38
Avinash Wakle <avinashwakle06@gmail.com>
2026-09-02 08:03:53 +0530

    Addition and subtraction multipliacationn

---
+++ Introduction 1st projects/multi.js
@@ -0,0 +1,4 @@
+console.log(24*84);
+console.log(35*20);
+console.log(100*50);    
+console.log(3333333*273634);
---
+++ Introduction 1st projects/sub.js
@@ -0,0 +1,4 @@
+console.log(23-33);
+console.log(50-20);
+console.log(100-50);
+console.log(3333333-273634);
