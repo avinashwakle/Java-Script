b4c3014995ec0d23e34a2061affd022f3821d995
Avinash Wakle <avinashwakle06@gmail.com>
2026-09-02 08:02:55 +0530

    Create 01.js

---
+++ Introduction 1st projects/01.js
@@ -0,0 +1,6 @@
+console.log("Hii im raj im learning a  new language called javascript");
+console.log(20+40);
+ console.log("--------------------------------------------------------- ");
+ console.log("my age is 20");
+ console.log("Im making a addition example for learning purpose");
+ console.log(450+5);
