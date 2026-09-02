fe2cb71f81c10149ff659834614ae7609f5b18dc
Avinash Wakle <avinashwakle06@gmail.com>
2026-09-02 16:30:10 +0530

    Create datatypes.js

---
+++ Data Types /datatypes.js
@@ -0,0 +1,16 @@
+let name ="White Tiger";
+let age = 5;
+let isStudent = true;
+let address =null;
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Is Student:", isStudent);
+console.log("Address:", address);
+
+console.log("------------------------------");
+
+console.log("Type of Name:", typeof name);
+console.log("Type of Age:", typeof age);
+console.log("Type of Is Student:", typeof isStudent);
+console.log("Type of Address:", typeof address);
