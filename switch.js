137c477b454bfe7eb268896fb3f0dc0161e82306
Avinash Wakle <avinashwakle06@gmail.com>
2026-09-07 20:58:47 +0530

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,39 @@
+let id = 4;
+switch (id) {
+    case 1:
+        console.log("Ram");
+        break;
+
+        case 2:
+        console.log("Shyam");
+        break;  
+        case 3:
+        console.log("Mohan");
+        break;
+        case 4:
+        console.log("Sohan");
+        break;
+    default:
+        console.log("No name found");
+        break;
+}
+
+let language = "JavaScript";
+
+switch (language) {
+
+    case "HTML":
+        console.log("HTML is used for structure");
+        break;
+
+    case "CSS":
+        console.log("CSS is used for styling");
+        break;
+
+    case "JavaScript":
+        console.log("JavaScript is used for logic");
+        break;
+
+    default:
+        console.log("Unknown Language");
+}
