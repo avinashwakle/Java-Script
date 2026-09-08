2b6dbf97a8e144b1c8f2ff0b17958b1ad0392d94
Avinash Wakle <avinashwakle06@gmail.com>
2026-09-08 11:45:09 +0530

    in this array the valuse change replays

---
+++ Array/arraych.js
@@ -0,0 +1,6 @@
+let fruits = ["apple", "banana", "cherry", "date", "elderberry"];
+
+fruits[3] = "dragonfruit";
+console.log(fruits);
+
+console.log("In this array the valuse is changesd from date to dragonfruit");
