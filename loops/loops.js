a5debda8ea66770a8d473b73f125acd7590ede08
Avinash Wakle <avinashwakle06@gmail.com>
2026-09-08 07:48:17 +0530

    Create loops.js

---
+++ loops/loops.js
@@ -0,0 +1,9 @@
+console.log("Im learning a Loops  in java script");
+console.log("For,while & Do while loops are learning in java script");
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+for ( let i = 1; i <= 100; i++) {
+    console.log(i);
+}
