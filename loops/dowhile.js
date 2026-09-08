384e5c1e2b3046d721b7c4bcfdd2389842d29b22
Avinash Wakle <avinashwakle06@gmail.com>
2026-09-08 07:48:57 +0530

    Add files via upload

---
+++ loops/dowhile.js
@@ -0,0 +1,9 @@
+console.log("Im learning a Loops  in java script");
+console.log("For,while & Do while loops are learning in java script");
+
+let i = 10;
+
+do {
+    console.log(i);
+    i++;
+} while (i <= 5);
---
+++ loops/while.js
@@ -0,0 +1,18 @@
+console.log("Im learning a Loops  in java script");
+console.log("For,while & Do while loops are learning in java script");
+
+
+
+
+let i = 1;
+
+
+while (i <= 5) {
+    console.log(i);
+    i++;
+}
+
+while (i<=100) {
+    console.log(i);
+    i++;
+}
